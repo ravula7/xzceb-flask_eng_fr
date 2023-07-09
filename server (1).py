4832e79d48c5d594c0378e8d7743fef59110d3c7
ravula7 <69436086+ravula7@users.noreply.github.com>
2023-07-09 15:33:15 -0400

    Add files via upload

---
+++ server (1).py
@@ -0,0 +1,25 @@
+import machinetranslation
+from machinetranslation import translator
+from flask import Flask, render_template, request
+import json
+
+app = Flask("Web Translator")
+
+@app.route("/englishToFrench")
+def englishToFrench():
+    textToTranslate = request.args.get('textToTranslate')
+    translator.__english_to_french__(textToTranslate)
+    return "Translated text to French"
+
+@app.route("/frenchToEnglish")
+def frenchToEnglish():
+    textToTranslate = request.args.get('textToTranslate')
+    translator.__french_to_english__(textToTranslate)
+    return "Translated text to English"
+
+@app.route("/")
+def renderIndexPage():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
